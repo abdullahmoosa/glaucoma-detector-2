eba0b9c81606271b664e0dc98661ba38d4a28d75
Abdullah <abdullahmoosa128@gmail.com>
2023-09-10 23:46:26 +0600

    Edited app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
     config['preauthorized']
 )
-model = tf.keras.models.load_model(r'glaucoma_detector\models\raw-test1.h5', compile = False)
+model = tf.keras.models.load_model(r'models\raw-test1.h5', compile = False)
 # Define a function to make predictions on the input image
 def predict_glaucoma(image):
     # Preprocess the image (resize, normalize, etc. based on your model's requirements)
